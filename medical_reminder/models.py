b46f0c835858640e670c18db42217653278ee975
basnaly <basnaly@gmail.com>
2024-02-03 19:31:05 +0200

    Create medicine model, layout.html, styles,

--- medical_reminder/models.py
+++ medical_reminder/models.py
@@ -9,3 +9,12 @@ class User(AbstractUser):
     def __str__(self):
         return f'{self.username}, {self.first_name}, {self.last_name}, {self.email}'
     
+class Medicine(models.Model):
+    name = models.CharField(max_length=120)
+    form = models.CharField(max_length=60)
+    strength = models.FloatField(blank=True, null=True)
+    unit = models.CharField(max_length=20)
+    frequency = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f'{self.name}, {self.form}, {self.strength}, {self.unit}, {self.frequency}'
